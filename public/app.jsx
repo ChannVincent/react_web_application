bb475b3d6c2269dfb23a4642873fa0119143c9bd
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-04-06 22:58:39 +0200

    RW-2 : components : form onSubmit callback

--- public/app.jsx
+++ public/app.jsx
@@ -1,24 +1,34 @@
 var Greeter = React.createClass({
   getDefaultProps: function () {
     return {
-      name: 'React'
+      name: 'React',
+      message: 'This is from the component'
     }
   },
+  onButtonClick: function (event) {
+    event.preventDefault();
+    var name = this.refs.name.value;
+    alert(name);
+  },
   render: function () {
-    var name = this.props.name;
-
+    const { name, message } = this.props;
     return (
       <div>
-        <h1>Hello {name} !</h1>
-        <p>This is from the component</p>
+        <h1>Hello { name }!</h1>
+        <p>{ message }</p>
+
+      <form onSubmit={ this.onButtonClick }>
+        <input type="text" ref="name"/>
+        <button>Send</button>
+      </form>
       </div>
     );
   }
 });
 
 var firstName = 'Andrew';
-
+var message = 'Hello you';
 ReactDOM.render(
-  <Greeter name={ firstName } />,
+  <Greeter name={ firstName } message={ message } />,
   document.getElementById("app")
 );
