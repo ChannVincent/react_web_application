b5f1511ba71073663e23f8fe0fb0e4f763ce322a
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-04-07 23:57:58 +0200

    RW-2 : components : 1st challenge

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,4 @@
+// result component
 var GreeterMessage = React.createClass({
   render: function () {
     const { name, message } = this.props;
@@ -10,25 +11,33 @@ var GreeterMessage = React.createClass({
   }
 })
 
+// form component
 var GreeterForm = React.createClass({
   onFormSubmit: function (event) {
     event.preventDefault();
     var name = this.refs.name.value;
+    var message = this.refs.message.value;
     if (name.length > 0) {
       this.refs.name.value = '';
       this.props.onNewName(name);
+    }
+    if (message.length > 0) {
+      this.refs.message.value = '';
+      this.props.onNewMessage(message);
     }
   },
   render: function () {
     return (
       <form onSubmit={ this.onFormSubmit }>
         <input type="text" ref="name"/>
+        <input type="text" ref="message"/>
         <button>Send</button>
       </form>
     )
   }
 })
 
+// container component
 var Greeter = React.createClass({
   getDefaultProps: function () {
     return {
@@ -44,20 +53,26 @@ var Greeter = React.createClass({
   },
   handleNewName: function (name) {
     this.setState({
-      name: name
+      name
+    })
+  },
+  handleNewMessage: function (message) {
+    this.setState({
+      message
     })
   },
   render: function () {
     const { name, message } = this.state;
     return (
       <div>
-      <GreeterMessage name={ name } message={ message }/>
-      <GreeterForm onNewName={ this.handleNewName } />
+        <GreeterMessage name={ name } message={ message }/>
+        <GreeterForm onNewName={ this.handleNewName } onNewMessage={ this.handleNewMessage } />
       </div>
     );
   }
 });
 
+// root
 var firstName = 'Andrew';
 var message = 'Hello you';
 ReactDOM.render(
