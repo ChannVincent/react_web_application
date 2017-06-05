67eaf5ea322f0f7290b5927255a36587088d9905
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-06-05 11:37:55 +0200

    RW-3 : a better react app : experimental es6

--- 03_react_experimental_js/app/app.jsx
+++ 03_react_experimental_js/app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name: 'Andrew',
+  location: 'Philadelphia'
+}
+
+var objeTwo = {
+  age: 25,
+  ...objOne
+}
+
+console.log(objeTwo);
+
 ReactDOM.render(
   <h1>Boilerplate app!</h1>,
   document.getElementById("app")
