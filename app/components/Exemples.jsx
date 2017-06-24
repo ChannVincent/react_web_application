36ba15a67ea7392d4c20e785fd913eb7cc9a94b9
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-06-24 16:08:51 +0200

    RW-5 : styling : gridview

--- app/components/Exemples.jsx
+++ app/components/Exemples.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+var {
+  Link
+} = require('react-router');
 
 var Exemples = (props) => {
   return (
-    <h3>Exemples Component</h3>
+    <div>
+
+      <h1 className="text-center">Exemples</h1>
+      <p>Here are a few example locations to try out:</p>
+
+      <ul>
+        <li>
+          <Link to="/?location=Philadelphia">Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio">Rio, Brazil</Link>
+        </li>
+      </ul>
+
+    </div>
   );
 }
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,11 @@ var Main = (props) => {
   return (
     <div>
       <Nav />
-      <p> ====================================================== </p>
-      { props.children }
+      <div className="row">
+        <div className="column medium-6 large-4 small-centered">
+          { props.children }
+        </div>
+      </div>
     </div>
   );
 }
