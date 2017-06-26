3d4f717feac481a65f67f77aa45b4ac3cb6c6625
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-06-26 22:50:04 +0200

    RW-5 : styling : About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,21 @@ import React from 'react';
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>Thi is a weather application built on React.</p>
+      <p>
+        Here are some of the tool I use :
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.ios/react">React </a>
+             - This was the javascript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map </a>
+             - I used Open Weather map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   );
 };
