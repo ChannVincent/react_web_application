f4a5454a9e890a89805e65a240e1eba8445d718e
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-08-01 22:17:31 +0200

    RW-6 : timer app : multiple components

--- app/app.jsx
+++ app/app.jsx
@@ -7,6 +7,8 @@ var {
   hashHistory
 } = require('react-router');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 // load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
@@ -16,9 +18,10 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <Router history={ hashHistory }>
-    <Route path="/" component={ Main }>
-    
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="countdown" component={Countdown}/>
+      <IndexRoute component={Timer}/>
     </Route>
   </Router>,
   document.getElementById("app")
